149af6921a294b14c7007849f9b0a287f11181b1
Divyanshu <divyanshukasoudhan3@gmail.com>
2025-08-22 13:24:27 +0530

    third commit

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ document.addEventListener('keydown', event => {
         event.preventDefault();
         calculator.compute();
         calculator.updateDisplay();
-    } else if (event.key === 'Backspaces') {
+    } else if (event.key === 'Backspace') {
         calculator.delete();
         calculator.updateDisplay();
     } else if (event.key === 'Escape') {
